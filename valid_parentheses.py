741e2ac33b54ec250845d28ba8e95b49d60a1534
Namir50 <90313634+Namir50@users.noreply.github.com>
2024-06-06 01:03:00 +0530

    Create valid_parentheses.py

---
+++ valid_parentheses.py
@@ -0,0 +1,40 @@
+'''Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false'''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        for char in s:
+            if char in bracket_map.values():
+                stack.append(char)
+            elif char in bracket_map: 
+                if stack and stack[-1] == bracket_map[char]:
+                    stack.pop()  
+                else:
+                    return False 
+            else:
+                return False 
+
+        return not stack
+
